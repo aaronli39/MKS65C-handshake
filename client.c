32846dc36d5a0b4a2c3bb51f1ff63167d67fd84f
Aaron Li <aaronli39@gmail.com>
2018-12-19 22:14:11 -0500

    finished

--- client.c
+++ client.c
@@ -1,10 +1,50 @@
+#include <string.h>
+#include <signal.h>
+
 #include "pipe_networking.h"
 
+int to_server;
+int from_server = -1;
 
-int main() {
+void sighandler(int s)
+{
+    switch (s)
+    {
+        case SIGINT:
+        remove("publicPipe");
+        exit(0);
+        case SIGPIPE:
+        close(to_server);
+        printf("Connection to server ended.\n");
+        exit(0);
+    }
+}
 
-  int to_server;
-  int from_server;
+int main()
+{
+    char buf[256];
 
-  from_server = client_handshake( &to_server );
+    signal(SIGINT, sighandler);
+    signal(SIGPIPE, sighandler);
+
+    for (;;)
+    {
+        while (from_server == -1)
+        {
+            from_server = client_handshake(&to_server);
+            if (from_server == -1)
+            {
+                printf("Error occurred. Retrying handshake in 3 seconds\n");
+                sleep(3);
+            }
+        }
+
+        memset(buf, 0, 256);
+        printf("\n--- Input message(type it and press enter) ---\n");
+        read(STDIN_FILENO, buf, 256);
+        buf[strlen(buf) - 1] = 0;
+        write(to_server, buf, 256);
+        read(from_server, buf, 256);
+        printf("\n--- Server response ---\n%s\n", buf);
+    }
 }
